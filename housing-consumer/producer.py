ff0c342cdbbd4d6c1e6c7510b9423a6203599761
adam <adamezouhairi@outlook.fr>
2025-03-02 15:04:08 +0000

    finish

--- housing-consumer/producer.py
+++ housing-consumer/producer.py
@@ -1,8 +1,20 @@
 import json
 from confluent_kafka import Producer
 
-KAFKA_BROKER = "localhost:29092"
-producer = Producer({'bootstrap.servers': KAFKA_BROKER})
+KAFKA_BROKER = "broker:29092"
+KAFKA_TOPIC = "housing_topic"
+
+producer_conf = {
+    'bootstrap.servers': KAFKA_BROKER
+}
+
+producer = Producer(producer_conf)
+
+def delivery_report(err, msg):
+    if err:
+        print("[producer] Échec de livraison:", err)
+    else:
+        print(f"[producer] Message livré sur {msg.topic()} partition {msg.partition()}")
 
 message = {
     "longitude": -122.23,
@@ -17,6 +29,8 @@ message = {
     "ocean_proximity": "NEAR BAY"
 }
 
-producer.produce('housing_topic', json.dumps(message).encode('utf-8'))
+message_str = json.dumps(message)
+producer.produce(KAFKA_TOPIC, message_str.encode('utf-8'), callback=delivery_report)
 producer.flush()
-print("Message envoyé")
+
+print("[producer] Message envoyé.")
